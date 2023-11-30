0bf8897bcf80067d72c6692feb1f8e9cd375662b
Franco Antonio Pranata <francopranara@gmail.com>
2023-11-30 15:48:51 +0700

    update

--- FnB_OOP/src/FnB_Main/Customer.java
+++ FnB_OOP/src/FnB_Main/Customer.java
@@ -1,45 +1,208 @@
+// package FnB_Main;
+
+// import java.sql.Connection;
+// import java.sql.PreparedStatement;
+// import java.sql.ResultSet;
+// import java.sql.SQLException;
+
+// //Kelas abstrak sebagai dasar untuk objek pelanggan
+
+// public class Customer implements CustomerManagement {
+//     private String name;
+//     private String address;
+//     private double balance;
+    
+//  // constructor, getter, setter methods
+// 	public Customer() {
+		
+// 	}
+
+// 	protected String getName() {
+// 		return name;
+// 	}
+
+// 	protected void setName(String name) {
+// 		this.name = name;
+// 	}
+
+// 	protected String getAddress() {
+// 		return address;
+// 	}
+
+// 	protected void setAddress(String address) {
+// 		this.address = address;
+// 	}
+
+// 	protected double getBalance() {
+// 		return balance;
+// 	}
+
+// 	protected void setBalance(double balance) {
+// 		this.balance = balance;
+// 	}
+
+
+//     @Override
+//     public void addCustomer(String name, String address, double initialBalance) throws SQLException {
+//         try (Connection connection = DatabaseConnection.getConnection()) {
+//             String query = "INSERT INTO customer (name, address, balance) VALUES (?, ?, ?)";
+//             try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+//                 preparedStatement.setString(1, name);
+//                 preparedStatement.setString(2, address);
+//                 preparedStatement.setDouble(3, initialBalance);
+
+//                 preparedStatement.executeUpdate();
+//             }
+//         }
+//     }
+
+//     // @Override
+//     // public void updateCustomer(String name, String address) throws SQLException {
+//     //     try (Connection connection = DatabaseConnection.getConnection()) {
+//     //         String query = "UPDATE customer SET address = ? WHERE name = ?";
+//     //         try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+//     //             preparedStatement.setString(1, address);
+//     //             preparedStatement.setString(2, name);
+
+//     //             preparedStatement.executeUpdate();
+//     //         }
+//     //     }
+//     // }
+
+//     @Override
+//     public void updateCustomer(String name, String newAddress) throws SQLException {
+//         try (Connection connection = DatabaseConnection.getConnection()) {
+//             String query = "UPDATE customer SET address = ? WHERE name = ?";
+//             try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+//                 preparedStatement.setString(1, newAddress);
+//                 preparedStatement.setString(2, name);
+
+//                 int rowsAffected = preparedStatement.executeUpdate();
+
+//                 if (rowsAffected > 0) {
+//                     System.out.println("Data pelanggan berhasil diperbarui.");
+//                 } else {
+//                     System.out.println("Data pelanggan tidak ditemukan.");
+//                 }
+//             }
+//         }
+//     }
+
+//     @Override
+//     public void deleteCustomer(String name) throws SQLException {
+//         try (Connection connection = DatabaseConnection.getConnection()) {
+//             String query = "DELETE FROM customer WHERE name = ?";
+//             try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+//                 preparedStatement.setString(1, name);
+
+//                 preparedStatement.executeUpdate();
+//             }
+//         }
+//     }
+	
+//     // @Override
+//     public static Customer getCustomerByName(String name) throws SQLException {
+//         try (Connection connection = DatabaseConnection.getConnection()) {
+//             String query = "SELECT * FROM customer WHERE name = ?";
+//             try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+//                 preparedStatement.setString(1, name);
+
+//                 try (ResultSet resultSet = preparedStatement.executeQuery()) {
+//                     if (resultSet.next()) {
+//                         Customer customer = new Customer();
+//                         customer.setName(resultSet.getString("name"));
+//                         customer.setAddress(resultSet.getString("address"));
+//                         customer.setBalance(resultSet.getDouble("balance"));
+//                         return customer;
+//                     }
+//                 }
+//             }
+//         }
+//         return null;
+//     }
+
+//     public void viewAllCustomers() throws SQLException {
+//         try (Connection connection = DatabaseConnection.getConnection()) {
+//             String query = "SELECT * FROM customer";
+//             try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+//                 try (ResultSet resultSet = preparedStatement.executeQuery()) {
+//                     System.out.println("List of all customers:");
+
+//                     while (resultSet.next()) {
+//                         System.out.println("Name: " + resultSet.getString("name"));
+//                         System.out.println("Address: " + resultSet.getString("address"));
+//                         System.out.println("Balance: " + resultSet.getDouble("balance"));
+//                         System.out.println("-------------------------");
+//                     }
+//                 }
+//             }
+//         }
+//     }
+
+//     public void topUpBalance(String name, double amount) {
+//         try (Connection connection = DatabaseConnection.getConnection()) {
+//             String query = "UPDATE customer SET balance = balance + ? WHERE name = ?";
+//             try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+//                 preparedStatement.setDouble(1, amount);
+//                 preparedStatement.setString(2, name);
+
+//                 int rowsAffected = preparedStatement.executeUpdate();
+
+//                 if (rowsAffected > 0) {
+//                     System.out.println("Saldo berhasil ditambahkan.");
+//                 } else {
+//                     System.out.println("Gagal menambahkan saldo.");
+//                 }
+//             }
+//         } catch (SQLException e) {
+//             System.out.println("Error topping up balance: " + e.getMessage());
+//         }
+//     }
+// }
+
+
 package FnB_Main;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-//Kelas abstrak sebagai dasar untuk objek pelanggan
+// Kelas abstrak sebagai dasar untuk objek pelanggan
 
 public class Customer implements CustomerManagement {
     private String name;
     private String address;
     private double balance;
-    
- // constructor, getter, setter methods
-	public Customer() {
-		
-	}
 
-	protected String getName() {
-		return name;
-	}
+    // constructor, getter, setter methods
+    public Customer() {
 
-	protected void setName(String name) {
-		this.name = name;
-	}
+    }
 
-	protected String getAddress() {
-		return address;
-	}
+    protected String getName() {
+        return name;
+    }
 
-	protected void setAddress(String address) {
-		this.address = address;
-	}
+    protected void setName(String name) {
+        this.name = name;
+    }
 
-	protected double getBalance() {
-		return balance;
-	}
+    protected String getAddress() {
+        return address;
+    }
 
-	protected void setBalance(double balance) {
-		this.balance = balance;
-	}
+    protected void setAddress(String address) {
+        this.address = address;
+    }
 
+    protected double getBalance() {
+        return balance;
+    }
+
+    protected void setBalance(double balance) {
+        this.balance = balance;
+    }
 
     @Override
     public void addCustomer(String name, String address, double initialBalance) throws SQLException {
@@ -56,14 +219,20 @@ public class Customer implements CustomerManagement {
     }
 
     @Override
-    public void updateCustomer(String name, String address) throws SQLException {
+    public void updateCustomer(String name, String newAddress) throws SQLException {
         try (Connection connection = DatabaseConnection.getConnection()) {
             String query = "UPDATE customer SET address = ? WHERE name = ?";
             try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-                preparedStatement.setString(1, address);
+                preparedStatement.setString(1, newAddress);
                 preparedStatement.setString(2, name);
 
-                preparedStatement.executeUpdate();
+                int rowsAffected = preparedStatement.executeUpdate();
+
+                if (rowsAffected > 0) {
+                    System.out.println("Data pelanggan berhasil diperbarui.");
+                } else {
+                    System.out.println("Data pelanggan tidak ditemukan.");
+                }
             }
         }
     }
@@ -79,6 +248,61 @@ public class Customer implements CustomerManagement {
             }
         }
     }
-	
 
+    public static Customer getCustomerByName(String name) throws SQLException {
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            String query = "SELECT * FROM customer WHERE name = ?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                preparedStatement.setString(1, name);
+
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    if (resultSet.next()) {
+                        Customer customer = new Customer();
+                        customer.setName(resultSet.getString("name"));
+                        customer.setAddress(resultSet.getString("address"));
+                        customer.setBalance(resultSet.getDouble("balance"));
+                        return customer;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public void viewAllCustomers() throws SQLException {
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            String query = "SELECT * FROM customer";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    System.out.println("List of all customers:");
+
+                    while (resultSet.next()) {
+                        System.out.println("Name: " + resultSet.getString("name"));
+                        System.out.println("Address: " + resultSet.getString("address"));
+                        System.out.println("Balance: " + resultSet.getDouble("balance"));
+                        System.out.println("-------------------------");
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void topUpBalance(String name, double amount) throws SQLException {
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            String query = "UPDATE customer SET balance = balance + ? WHERE name = ?";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                preparedStatement.setDouble(1, amount);
+                preparedStatement.setString(2, name);
+
+                int rowsAffected = preparedStatement.executeUpdate();
+
+                if (rowsAffected > 0) {
+                    System.out.println("Saldo berhasil ditambahkan.");
+                } else {
+                    System.out.println("Gagal menambahkan saldo.");
+                }
+            }
+        }
+    }
 }
--- FnB_OOP/src/FnB_Main/CustomerManagement.java
+++ FnB_OOP/src/FnB_Main/CustomerManagement.java
@@ -9,5 +9,10 @@ public interface CustomerManagement {
     void addCustomer(String name, String address, double initialBalance) throws SQLException;
     void updateCustomer(String name, String address) throws SQLException;
     void deleteCustomer(String name) throws SQLException;
-    
+    static Customer getCustomerByName(String name) throws SQLException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCustomerByName'");
+    }
+    void topUpBalance(String name, double amount) throws SQLException;
+    // void viewAllCustomers();
 }
