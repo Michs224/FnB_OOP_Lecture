422ae64f3c9f98b1255c91bd78a2358252a266ab
Michael Geraldin Wijaya <michaelwjy2@gmail.com>
2023-12-18 19:39:43 +0700

    Updated

--- FnB_OOP/src/FnB_Display/Display.java
+++ FnB_OOP/src/FnB_Display/Display.java
@@ -2,9 +2,10 @@ package FnB_Display;
 
 public class Display {
     public void mainMenu() {
-        System.out.println("               EATEN              ");
-        System.out.println("Aplikasi Pesan Makanan dan Minuman");
-        System.out.println("----------------------------------");
+    	System.out.println("-----------------------------");
+        System.out.println("------------------------- EATEN -------------------------");
+        System.out.println("|Aplikasi Pesan Makanan dan Minuman          |");
+        System.out.println("---------------------------------------------------------");
         System.out.println("1. Manajemen data pelanggan");
         System.out.println("2. Katalog makanan dan minuman");
         System.out.println("3. Top up saldo");
