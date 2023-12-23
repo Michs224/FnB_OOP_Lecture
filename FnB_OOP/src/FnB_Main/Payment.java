1c5ce525492110588e6d810f489ad54e7ab2bc2c
Michael Geraldin Wijaya <128117104+Michs224@users.noreply.github.com>
2023-12-23 18:42:01 +0700

    Delete FnB_OOP directory

--- FnB_OOP/src/FnB_Main/Payment.java
+++
@@ -1,9 +0,0 @@
-package FnB_Main;
-
-import java.sql.SQLException;
-
-//Interface untuk manajemen pembayaran
-
-public interface Payment {
-	void makePayment(Customer customer, double amount) throws SQLException;
-}
